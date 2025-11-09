ec06882958286706908aae920aac66c0d12ed8ea
Proclus Harmund <procluha@ge-n5.kzn.21-school.ru>
2025-11-09 10:50:09 +0300

    added support for all data types

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,6 +1,5 @@
 #include "s21_sprintf.h"
 
-
 int main() {
     char str[150], str1[150], str2[150], str3[150];
     s21_sprintf(str, "macan %s %c %u %% %d %f %f %.3f", "frontender", 'r', 10, -10, 4.543, -4.543, 982341.12345);
@@ -9,13 +8,19 @@ int main() {
     sprintf(str2, "macan %s %c %u %% %d %f %f %.3f", "frontender", 'r', 10, -10, 4.543, -4.543, 982341.12345);
     sprintf(str3, "%+d % d %.4f", 10, -10, 4.543);
 
-    printf("==S21_SPRINTF OUTPUT===\n%s\n%s\n===SYSTEM SPRINTF OUTPUT===\n%s\n%s", str,str1,str2,str3);
+    printf("\n===S21_SPRINTF OUTPUT===\n%s\n%s\n\n===SYSTEM SPRINTF OUTPUT===\n%s\n%s", str,str1,str2,str3);
 
-    char sigor[100], sigor1[100];
-    s21_sprintf(sigor, "Arbuz арбуз %d %.7f %s", 1488, 14.81337228, "macan huesos");
-    sprintf(sigor1, "Arbuz арбуз %d %.7f %s", 1488, 14.81337228, "macan huesos");
-    printf("\n%s\n%s", sigor, sigor1);
+    short min_short = SHRT_MIN, max_short = SHRT_MAX;
+    unsigned short max_unsigned_short = USHRT_MAX;
+    int min_int = INT_MIN, max_int = INT_MAX;
+    unsigned int max_unsigned_int = UINT_MAX;
+    long min_long = LONG_MIN, max_long = LONG_MAX;
+    unsigned long max_unsigned_long = ULONG_MAX;
 
+    char c1[500], c2[500];
+    s21_sprintf(c1, "short: %hd %hd\nunsigned short %hu\nint %d %d\nunsigned int %u\nlong %ld %ld\nunsigned long %lu",min_short,max_short,max_unsigned_short, min_int, max_int, max_unsigned_int, min_long, max_long, max_unsigned_long);
+    sprintf(c2, "short: %hd %hd\nunsigned short %hu\nint %d %d\nunsigned int %u\nlong %ld %ld\nunsigned long %lu",min_short,max_short,max_unsigned_short, min_int, max_int, max_unsigned_int, min_long, max_long, max_unsigned_long);
+    printf("\n\n===S21 DIFF DATA TYPE OUTPUT===\n%s\n\n===SYSTEM DIFF DATA TYPE OUTPUT===\n%s\n", c1, c2);
     return 0;
 }
 
@@ -35,14 +40,16 @@ void parse_format(char* str, const char *format, va_list* args_ptr){
         if (flag_container.is_percent){
             flag_container.is_percent = 0;
             if (*p == 'd'){
-                int arg = va_arg(*args_ptr, int), tmp = arg, count;     //берем аргумент
-                for (count = 0; tmp != 0; count++) tmp/=10;             //считаем кол-во цифр
-                if (count == 0) count+=2;
-                char* new_str = malloc(sizeof(char)*count);             
-                int_to_str(arg, new_str, flag_container.need_sign, count);
-                add_to_dest(str, new_str);
-                free(new_str);
-                flag_container.need_sign = 0;       //по дефолту сбрасываем флаг
+                long long val;
+                    if (flag_container.is_short) {
+                        int tmp = va_arg(*args_ptr, int);  //повышаем в инт
+                        val = (short)tmp;                  //и снова делаем шортом
+                    } else
+                        val = va_arg(*args_ptr, long long);      // обычный int
+                add_to_dest(str, int_to_str(val, flag_container.need_sign));
+                flag_container.need_sign = 0;
+                flag_container.is_short = 0;
+                flag_container.is_long = 0;
             } else if (*p == 's') {
                 add_to_dest(str, va_arg(*args_ptr, char*));
             } else if (*p == '%') {
@@ -51,13 +58,13 @@ void parse_format(char* str, const char *format, va_list* args_ptr){
                 char buf[2]; buf[1] = '\0'; buf[0] = (char)va_arg(*args_ptr, int);
                 add_to_dest(str, buf);
             } else if (*p == 'u'){
-                int arg = va_arg(*args_ptr, unsigned int), tmp = arg, count;        //тоже самое что и int
-                for (count = 0; tmp != 0; count++) tmp/=10;
-                char* new_str = malloc(sizeof(char)*count);
-                int_to_str(arg, new_str, flag_container.need_sign, count);
-                add_to_dest(str, new_str);
-                free(new_str);
+                if (flag_container.is_long)
+                    add_to_dest(str, unsigned_long_to_str(va_arg(*args_ptr, unsigned long long), flag_container.need_sign));
+                else
+                    add_to_dest(str, int_to_str(va_arg(*args_ptr, unsigned int), flag_container.need_sign));
                 flag_container.need_sign = 0;
+                flag_container.is_short = 0;
+                flag_container.is_long = 0;
             } else if (*p == 'f'){
                 float_to_str(str, va_arg(*args_ptr, double), flag_container.float_accuracy, flag_container.need_sign);  //по дефолту va_arg возвращает дабл
                 flag_container.float_accuracy = 6;
@@ -78,6 +85,15 @@ void parse_format(char* str, const char *format, va_list* args_ptr){
                 flag_container.need_sign = -1;
                 flag_container.is_percent = 1;
             }
+            else if (*p == 'l'){
+                flag_container.is_long = 1;
+                flag_container.is_percent = 1;
+            }
+            else if (*p == 'h'){
+                flag_container.is_percent = 1;
+                flag_container.is_short = 1;
+            }
+            
         }
         else if (*p == '%') {
             flag_container.is_percent = 1;
@@ -96,63 +112,91 @@ void add_to_dest(char* dest, char* str) {
     strcat(dest, str);
 }
 
-void int_to_str(int num, char* dest, int need_sign, int count) {
-    int is_negative = 0, i = 0;
-    if (num == 0) 
-        strcpy(dest, "0");
-    else{
-    if (num < 0){
-        num*=-1;
+char* unsigned_long_to_str(unsigned long long num, int need_sign){
+    char* buffer = malloc(sizeof(char*) * 30);      //20-21 цифра максимум, взял с запасом
+    if (!buffer) return NULL;
+    int i = 0;
+    if (num == 0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+    }
+    char temp[20];
+    int len = 0;
+    while (num > 0) {       //идем с конца, заносим в буфер
+        temp[len++] = '0' + (num % 10);
+        num /= 10;
+    }
+    if (need_sign == 1)
+        buffer[i++] = '+';
+    for (int j = len - 1; j >= 0; j--)        //копируем чары как цифры в строку
+        buffer[i++] = temp[j];
+    buffer[i] = '\0';
+    return buffer;
+}
+
+
+char* int_to_str(long long num, int need_sign) {
+    int is_minimal_negative_long = 0;
+    char* buffer = malloc(sizeof(char*) * 15);      //в инте 10-11 цифр максимум, взял с запасом
+    if (!buffer) return NULL;
+    int i = 0;
+    int is_negative = 0;
+    if (num == 0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+    }
+    if (num == LLONG_MIN) {
+        is_minimal_negative_long = 1;
+    }
+    if (num < 0) {
         is_negative = 1;
+        num = -num;
     }
-    char* buf = calloc(count + 1, sizeof(char));
-    while (num != 0) {      //идем с конца, заносим в буфер
-        buf[i] = '0' + num%10;
-        i++;
-        num/=10;
+    char temp[20];
+    int len = 0;
+    while (num > 0) {       //идем с конца, заносим в буфер
+        temp[len++] = '0' + (num % 10);
+        num /= 10;
     }
-    if (is_negative && need_sign >= 0) {        //добавляем знак если число < 0 и это разрешает флаг
-        buf[i++] = '-';
-        count++;
-    }
-    else if (need_sign == 1) {                  //принудительно добавляем флаг
-        buf[i++] = is_negative ? '-' : '+';
-        count++;
-    }
-    for (int i = 0; i < count; i++){            //копируем чары как цифры в строку
-        char tmp[2]; tmp[1] = '\0';
-        tmp[0] = buf[count - i -1];
-        strcat(dest, tmp);
-    }
-    free(buf);
-    }
+    if (is_negative && need_sign >= 0)    //добавляем знак если число < 0 и это разрешает флаг
+        buffer[i++] = '-';
+    else if (need_sign == 1 && !is_negative)
+        buffer[i++] = '+';
+    for (int j = len - 1; j >= 0; j--)        //копируем чары как цифры в строку
+        buffer[i++] = temp[j];
+    buffer[i] = '\0';
+    if (is_minimal_negative_long) strcpy(buffer, "-9223372036854775808");;
+    return buffer;
 }
 
 void float_to_str(char* dest, double num, int accuracy, int need_sign) {
-    int is_negative = 0, whole_count;
+    int is_negative = 0;
     if (num < 0) {
-        num*=-1;
         is_negative = 1;
+        num = -num;
     }
-    int whole_part = (int)num, tmp = whole_part;        //выделяем целую и дробную часть
-    float fractional_part = num - whole_part;
-
-    for (whole_count = 0; tmp != 0; whole_count++) tmp/=10;     //счет цифр в целой части и преобразование в инт
-    if (is_negative) whole_part*=-1;
-    int_to_str(whole_part, dest, need_sign, whole_count);       //запись в dest на лету*
-
-    int multiplier = 1;                                         //задаем множитель, по точности(дефолт 6)
-    for (int i = 0; i < accuracy; i++) {
-        multiplier *= 10;
+    int whole_part = (int)num;
+    double fractional_part = num - whole_part;
+    char* whole_str = int_to_str(whole_part, need_sign);        //преобразуем целую часть в инт
+    if (is_negative && need_sign >= 0) {
+        char* temp = malloc(strlen(whole_str) + 2);
+        temp[0] = '-';
+        strcpy(temp + 1, whole_str);
+        free(whole_str);
+        whole_str = temp;
     }
-    int fractional_int = (int)((fractional_part * multiplier) + 0.5f); //преобразуем дробную часть в инт(0.453 = 453)
+    strcat(dest, whole_str);
+    free(whole_str);
+    int multiplier = 1; //задаем множитель, по точности(дефолт 6)
+    for (int i = 0; i < accuracy; i++) multiplier *= 10;
+
+    int fractional_int = (int)((fractional_part * multiplier) + 0.5);//преобразуем дробную часть в инт(0.453 = 453)
                                                             //округление float вверх
-    char frac_str[10] = {0};
-    for (int i = accuracy - 1; i >= 0; i--) {                   //выделяем по цифрам и преобразуем в строку
+    char frac_str[15] = {0};
+    for (int i = accuracy - 1; i >= 0; i--) {           //выделяем по цифрам и преобразуем в строку
         frac_str[i] = '0' + (fractional_int % 10);
         fractional_int /= 10;
     }
-    strcat(dest, ".");                                          //копируем в dest
+    strcat(dest, ".");                                   //копируем в dest
     strcat(dest, frac_str);
-
 }
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -2,6 +2,8 @@
 #include <string.h> //заменить на s21_string.h
 #include <stdarg.h> //для обработки не определенного кол-ва аргументов функции
 #include <stdlib.h>
+#include <limits.h>         //либа для получения минимальных и максимальных значений в типов данных в системе
+
 
 #define s21_NULL ((void)*0)     //макрос для NULL
 
@@ -12,10 +14,13 @@ typedef struct {    //структура для флагов
     int need_sign;
     int wait_for_accuracy;
     int is_percent;
+    int is_long;
+    int is_short;
 } s21_format_t;
 
 void s21_sprintf(char *str, const char* format, ...);
 void parse_format(char* str, const char *format, va_list* args_ptr);
-void int_to_str(int num, char* dest, int need_sign, int count);
+char* int_to_str(long long num, int need_sign);
 void add_to_dest(char* dest, char* str);
 void float_to_str(char* dest, double num, int accuracy, int need_sign);
+char* unsigned_long_to_str(unsigned long long,int need_sign);
