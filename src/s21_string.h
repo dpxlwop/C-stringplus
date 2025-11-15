6ce2793d1b0febf2480bd059178d795b51e66826
Hilde Nathalie <hildenat@ge-n4.kzn.21-school.ru>
2025-11-15 18:00:31 +0300

    6 функций стрингов + хедер

--- src/s21_string.h
+++ src/s21_string.h
@@ -9,7 +9,7 @@ int s21_memcmp(const void *str1, const void *str2, s21_size_t n); //готова
 void *s21_memcpy(void *dest, const void *src, s21_size_t n); // готова 
 void *s21_memset(void *str, int c, s21_size_t n); //готова
 s21_size_t s21_strlen(const char *str); //готова
-char *s21_strncat(char *dest, const char *src, s21_size_t n); //готова (пока нет)
+char *s21_strncat(char *dest, const char *src, s21_size_t n); //готова
 char *s21_strchr(const char *str, int c);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
